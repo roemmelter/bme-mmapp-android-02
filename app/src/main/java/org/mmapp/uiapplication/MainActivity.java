51e9685fce2f52905023194e39663b2860184232
Erik Roemmelt <28647227+roemmelter@users.noreply.github.com>
2021-04-12 19:39:42 +0200

    Update ButtonActivity

--- app/src/main/java/org/mmapp/uiapplication/MainActivity.java
+++ app/src/main/java/org/mmapp/uiapplication/MainActivity.java
@@ -1,14 +1,79 @@
 package org.mmapp.uiapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+/**
+ * MIT License (http://choosealicense.com/licenses/mit/)
+ *
+ * MainActivity
+ *
+ * @author Erik Roemmelt
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private final String PACKAGE_NAME_BASE = "org.mmapp.";
+    private final String[] ACTIVITY_LIST = {
+            PACKAGE_NAME_BASE + "uiapplication." + "ButtonActivity",
+            PACKAGE_NAME_BASE + "uiapplication." + "DialogActivity",
+            PACKAGE_NAME_BASE + "uiapplication." + "InfoActivity"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        LinearLayout ll = new LinearLayout(this);
+        ll.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        ll.setBackgroundColor(0x20FFFFFF);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        for (String activity : ACTIVITY_LIST) {
+            ll.addView(createButtonForActivity(this, activity));
+        }
+
+        setContentView(ll);
+    }
+
+    protected Button createButtonForActivity(Context ctx, String activity) {
+        String btnLabel = activity.substring(activity.lastIndexOf(".")+1);
+        Button btn = new Button(ctx);
+        btn.setTypeface(Typeface.create(
+                "sans-serif", Typeface.NORMAL));
+        btn.setTransformationMethod(null);
+        btn.setText(btnLabel);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),
+                               "Button '" + btnLabel + "' was clicked",
+                               Toast.LENGTH_SHORT)
+                        .show();
+                try {
+                    Class<?> classToStart = Class.forName(activity);
+                    Intent intent = new Intent(ctx, classToStart);
+                    startActivity(intent);
+                } catch (ClassNotFoundException ignored) {
+                    Log.e("MainActivity", ignored.getException().toString());
+                    Toast.makeText(v.getContext(),
+                                   "Activity '" + btnLabel + "' not found.",
+                                   Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+        return btn;
     }
 }
